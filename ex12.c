f3909c3405e8a06099e602125430771c6e046dbf
LyazginD <lyazg10@mail.ru>
2024-06-30 20:10:08 +0500

    added exercise 13

--- ex12.c
+++ ex12.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-    int areas[] = { 10, 12, 13, 14, 20 };
-    char name[] = "Daniil";
     char full_name[] = {
         'D', 'a', 'n', 'i', 'i', 'l', ' ', 
         'S', '.', ' ',
         'L', 'y', 'a', 'z', 'g', 'i', 'n', '\0'
     };
+    int areas[] = { 10, 12, 13, 14, 20 };
+    char name[] = "Daniil";
+
+    name[4] = 69;
+    full_name[4] = 69;
 
     // WARNING: On some systems you may have to change the
     // %ld in this code to a %u since it will use unsigned ints
     printf("The size of an int: %ld\n", sizeof(int));
     printf("The size of areas (int[]): %ld\n", sizeof(areas));
     printf("The number of ints in areas: %ld\n", sizeof(areas) / sizeof(int));
-    printf("The first area is %d, the 2nd %d.\n", areas[0], areas[1]);
+    printf("The first area is %d, the 2nd is %d.\n", areas[0], areas[1]);
     
     printf("The size of a char: %ld\n", sizeof(char));
     printf("The size of name (char[]): %ld\n", sizeof(name));
---
+++ ex13.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int i = 0;
+
+
+    char *states[] = {
+        "California", "Oregon", "Texas", "Washington", NULL
+    };
+
+    int num_states = 5;
+
+    char tmp[] = "ass";
+    tmp[0] = *states[0];
+    states[0] = argv[1]; 
+    argv[1] = tmp;
+
+    for (i = 1; i <argc; i++) {
+        printf("arg %d: %s\n", i, argv[i]);
+    }
+
+    for (i = 0; i < num_states; i++) {
+        printf("state %d: %s\n", i, states[i]);
+    }
+
+    return 0;
+}
