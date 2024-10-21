741621c935fc8e987db36e809b7d154b7348e30c
LyazginD <lyazg10@mail.ru>
2024-10-21 12:31:34 +0500

    appended comments in exercise 17

--- ex17.c
+++ ex17.c
@@ -4,10 +4,12 @@
 #include <errno.h>
 #include <string.h>
 
-// constants
+// using c preprocessor here(cpp)
+// a way to create constants that will work reliably
 #define MAX_DATA 512
 #define MAX_ROWS 100
 
+// fixed sized structs are less efficient but it's easier to store and read them
 struct Address {
     int id;
     int set;
@@ -31,7 +33,8 @@ void die(const char *message) {
     if (errno) {
         // prints a system error message
         perror(message);
-    } else {
+    } else { 
+        // for custom errors
         printf("ERROR: %s\n", message);
     }
 
@@ -46,7 +49,8 @@ void Address_print(struct Address *addr) {
 
 // loads a database from the file
 void Database_load(struct Connection *conn) {
-    // reads 1 items of data, sizeof(struct Database) long, from the stream pointed to by conn->file, storing them at the conn->db
+    // reads 1 items of data, sizeof(struct Database) long, 
+    // from the stream pointed to by conn->file, storing them at the conn->db
     // on success return the number of items read
     int rc = fread(conn->db, sizeof(struct Database), 1, conn->file);
     if (rc != 1) {
@@ -66,12 +70,18 @@ struct Connection *Database_open(const char *filename, char mode) {
         die("Memory error.");
     }
 
+    // mode c is using for creating new file or rewriting existing one?
     if (mode == 'c') {
-        // fopen opens the file whose name is the string pointed to by filename and associates a stream with it. On successful completion return a FILE pointer. Otherwise, NULL is returned and erno is set to indicate the error
-        // w - truncate file to zero length or create text file for writing. The stream is positioned at the beginning of the file
+        // fopen opens the file whose name is the string pointed to by filename
+        // and associates a stream with it. 
+        // On successful completion return a FILE pointer. 
+        // Otherwise, NULL is returned and erno is set to indicate the error
+        // w - truncate file to zero length or create text file for writing. 
+        // The stream is positioned at the beginning of the file
         conn->file = fopen(filename, "w");
     } else {
-        // r+ - open for reading and writing. The stream is positioned at the beginning of the file
+        // r+ - open for reading and writing. 
+        // The stream is positioned at the beginning of the file
         conn->file = fopen(filename, "r+");
 
         if (conn->file) {
@@ -90,6 +100,7 @@ struct Connection *Database_open(const char *filename, char mode) {
 void Databae_close(struct Connection *conn) {
     if (conn) {
         if (conn->file) {
+            // Close stream
             fclose(conn->file);
         }
         if (conn->db) {
@@ -99,21 +110,28 @@ void Databae_close(struct Connection *conn) {
     }
 }
 
-// writes data to database
+// writes data from database to a file???
 void Database_write(struct Connection *conn) {
-    // rewind function sets the file position indicator for the stream pointed to by conn->file to the beginning of the file
+    // rewind function sets the file position indicator for the stream 
+    // pointed to by conn->file to the beginning of the file
     rewind(conn->file);
 
 
-    // fwrite writes 1 item of data, each sizeof(struct Database) long, to the stream pointed to by conn->file, obtaining them from the location given by conn->db
+    // fwrite writes 1 item of data, 
+    // each sizeof(struct Database) long, 
+    // to the stream pointed to by conn->file, 
+    // obtaining them from the location given by conn->db
     // Returns the number of written items 
     int rc = fwrite(conn->db, sizeof(struct Database), 1, conn->file);
     if (rc != 1) {
         die("Failed to write database.");
     }
 
-    // for output streams, fflush() forces a write of all user-space buffered data for the given output or update conn->file via the stream's underlying write function
-    // upon successful completion 0 is returned. Otherwise, EOF is returned and errno is set to indicate the error
+    // for output streams, 
+    // fflush() forces a write of all user-space buffered data for the given output 
+    // or update conn->file via the stream's underlying write function
+    // upon successful completion 0 is returned. 
+    // Otherwise, EOF is returned and errno is set to indicate the error
     rc = fflush(conn->file);
     if (rc == -1) {
         die("Cannot flush database.");
@@ -133,6 +151,7 @@ void Database_create(struct Connection *conn) {
 }
 
 void Database_set(struct Connection *conn, int id, const char *name, const char *email) {
+    // gets the address of rows[id]
     struct Address *addr = &conn->db->rows[id];
     if (addr->set) {
         die("Already set, delete it first");
@@ -140,10 +159,14 @@ void Database_set(struct Connection *conn, int id, const char *name, const char 
 
     addr->set = 1;
     // WARNING: bug
-    // strncpy copies at most MAX_DATA bytes of string pointed to by name, including the terminating null byte ('\0')
-    // returns a pointer to the destination string 
-    // The strings may not overlap, and the destination string must be large enough to receive the copy. BEWARE OF BUFFER OVERRUNS!
-    // Warning: if there is no null byte among the first MAX_DATA bytes of name, the string placed in dest will not be null-terminated
+    // strncpy copies at most MAX_DATA bytes of string pointed to by name, 
+    // including the terminating null byte ('\0')
+    // returns a pointer to the destination string.
+    // The strings may not overlap, 
+    // and the destination string must be large enough to receive the copy. 
+    // BEWARE OF BUFFER OVERRUNS!
+    // Warning: if there is no null byte among the first MAX_DATA bytes of name, 
+    // the string placed in dest will not be null-terminated
     char *res = strncpy(addr->name, name, MAX_DATA);
     // demonstrate this strncpy bug
     if (!res) {
@@ -156,6 +179,7 @@ void Database_set(struct Connection *conn, int id, const char *name, const char 
     }
 }
 
+// prints address by id 
 void Database_get(struct Connection *conn, int id) {
     struct Address *addr = &conn->db->rows[id];
 
@@ -166,11 +190,13 @@ void Database_get(struct Connection *conn, int id) {
     }
 }
 
+// marks address as not set 
 void Database_delete(struct Connection *conn, int id) {
     struct Address addr = {.id = id, .set = 0};
     conn->db->rows[id] = addr;
 }
 
+// print all addresses stored in db
 void Database_list(struct Connection *conn) {
     int i = 0;
     struct Database *db = conn->db;
@@ -184,6 +210,7 @@ void Database_list(struct Connection *conn) {
     }
 }
 
+// whe whole point of this program is to allocate a large amount of memory when creating the database
 int main(int argc, char **argv) {
     if (argc < 3) {
         die("USAGE: ex17 <dbfile> <action> [action params]");
@@ -235,6 +262,10 @@ int main(int argc, char **argv) {
 
     case 'l':
         Database_list(conn);
+        break;
+
+    case 'h':
+        die("Available actions: c=create, g=get, s=set, d=del, l=list.");
         break;
 
     default:
